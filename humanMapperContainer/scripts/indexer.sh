4506c1481fe8927ab92d8a3c4093900cca7c7797
franky2204 <fpantini1997@gmail.com>
2023-10-31 10:50:55 +0100

    errors in bash

--- humanMapperContainer/scripts/indexer.sh
+++ humanMapperContainer/scripts/indexer.sh
@@ -2,12 +2,12 @@ file=$1
 outputDb=$2
 method=$3
 
-if [$outputDb != ""]; then
-  if [$method = "is" || $method = "bwtsw"]; then
-    bwa index -p $outputDb -a $method $file
+if [ "$outputDb" != "" ]; then
+  if [ "$method" = "is" ] || [ "$method" = "bwtsw" ]; then
+    bwa index -p "$outputDb" -a "$method" "$file"
   else
-    bwa index -p $outputDb $file
+    bwa index -p "$outputDb" "$file"
   fi
 else
-  bwa index $file    
+  bwa index "$file"
 fi
