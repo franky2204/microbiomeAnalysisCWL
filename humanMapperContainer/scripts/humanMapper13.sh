72836db41e758018e16e6e91f4343f11b50e4448
franky2204 <fpantini1997@gmail.com>
2023-11-06 16:13:04 +0100

    prove

--- humanMapperContainer/scripts/humanMapper13.sh
+++ humanMapperContainer/scripts/humanMapper13.sh
@@ -9,8 +9,8 @@ threads=$4
 
 
 time {
-	bwa mem -t $threads $index $1 $2 > ${file_name}_pe_chm13.sam
-	samtools fastq -f 4 -@ $threads ${file_name}_pe_chm13.sam > ${file_name}_unmapped_chm13.fastq
+	bwa mem -t $threads $index $1 $2 > ${file_name}13_pe.sam
+	samtools fastq -f 4 -@ $threads ${file_name}13_pe.sam > ${file_name}_unmapped_chm13.fastq
 
 	python3 /scripts/divide_fastq.py ${file_name}_unmapped_chm13.fastq ${patient}_unmapped_R1_chm13.fastq ${patient}_unmapped_R2_chm13.fastq
 	gzip ${patient}_unmapped_R1_chm13.fastq
