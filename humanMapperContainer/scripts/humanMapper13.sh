d6913f4bfe9327f263a3cb5fb1706690922ec111
franky2204 <fpantini1997@gmail.com>
2023-11-15 20:26:49 +0100

    try #2

--- humanMapperContainer/scripts/humanMapper13.sh
+++ humanMapperContainer/scripts/humanMapper13.sh
@@ -10,9 +10,9 @@ threads=$4
 
 time {
 	bwa mem -t $threads $index $1 $2 > ${file_name}_pe.sam
-	samtools fastq -f 4 -@ $threads ${file_name}_pe.sam > ${file_name}_unmapped.fastq
+	samtools fastq -f 4 -@ $threads ${file_name}_pe.sam > ${file_name}_unmapthirtheen.fastq
 
-	python3 /scripts/divide_fastq.py ${file_name}_unmapped.fastq ${patient}_unmapped13_R1.fastq ${patient}_unmapped13_R2.fastq
-	gzip ${patient}_unmapped13_R1.fastq
-	gzip ${patient}_unmapped13_R2.fastq
+	python3 /scripts/divide_fastq.py ${file_name}_unmapthirtheen.fastq ${patient}_unmapthirtheen_R1.fastq ${patient}_unmapthirtheen_R2.fastq
+	gzip ${patient}_unmapthirtheen_R1.fastq
+	gzip ${patient}_unmapthirtheen_R2.fastq
 }
