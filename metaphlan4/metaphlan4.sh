0c4d2338d41ae39b3bb815b0d63fc5c3544a0f8d
francesco.pantini <francesco.pantini@edu.unito.it>
2024-02-05 16:03:00 +0100

    modified path to db

--- metaphlan4/metaphlan4.sh
+++ metaphlan4/metaphlan4.sh
@@ -2,4 +2,4 @@ read_1=$1
 filename=$(basename "$read_1")
 name=$(echo "$filename" | cut -d '_' -f 1)
 
-metaphlan $1,$2  --bowtie2out ${name}.bowtie2.bz2 --nproc $3 --input_type fastq -o ${name}_output.txt --bowtie2db 7c3cf4f35c85:/metaphlanDB
+metaphlan $1,$2  --bowtie2out ${name}.bowtie2.bz2 --nproc $3 --input_type fastq -o ${name}_output.txt --bowtie2db $4
