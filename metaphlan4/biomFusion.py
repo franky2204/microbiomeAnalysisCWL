97175c2917d69fcf92f162556f8a44ebab38186e
francesco.pantini <francesco.pantini@edu.unito.it>
2024-02-14 10:50:38 +0100

    new py

--- metaphlan4/biomFusion.py
+++ metaphlan4/biomFusion.py
@@ -3,14 +3,14 @@ import sys
 import csv
 from biom import load_table
 
-
-n=1
+n = 1  
 files = sys.argv[1:]
-final_table=load_table(file[0])
-files_len= len(files)
+final_table = load_table(files[0])  
+files_len = len(files)
+
 while n < files_len:
     table2 = load_table(files[n])
-    final_table = finalTable.merge(table2)
+    final_table = final_table.merge(table2)
     n += 1
 
 final_table.to_json('final_table.biom')
