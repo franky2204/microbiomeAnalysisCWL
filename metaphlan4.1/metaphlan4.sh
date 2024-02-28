5e8c861c6d1d3a4209ac01a1fe41e56fbae51455
francesco.pantini <francesco.pantini@edu.unito.it>
2024-02-28 21:25:11 +0100

    add

--- metaphlan4.1/metaphlan4.sh
+++
@@ -1,5 +0,0 @@
-read_1=$1
-filename=$(basename "$read_1")
-name=$(echo "$filename" | cut -d '_' -f 1)
-
-metaphlan $1,$2  --bowtie2out ${name}.bowtie2.bz2 --nproc $3 --input_type fastq -o ${name}_output.txt --biom ${name}_output.biom  --bowtie2db $4
