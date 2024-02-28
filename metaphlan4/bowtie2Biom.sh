db49988210cfab021978546bb49c568bb653e218
francesco.pantini <francesco.pantini@edu.unito.it>
2024-02-28 15:43:36 +0100

    fix

--- metaphlan4/bowtie2Biom.sh
+++ metaphlan4/bowtie2Biom.sh
@@ -1,5 +1,6 @@
 read_1=$1
 filename=$(basename "$read_1")
 name=$(echo "$filename" | cut -d '_' -f 1)
+name="${name%%.*}"
 
 metaphlan $1 --nproc $2 --input_type bowtie2out -o ${name}_output.txt --biom ${name}_output.biom  --bowtie2db $3
