ea12d83974af68978c1d649a4082e6ea737e4b56
francesco.pantini <francesco.pantini@edu.unito.it>
2024-03-29 15:50:42 +0100

    add workflow only kraken2

--- metaphlan4/bowtie2Biom.sh
+++ metaphlan4/bowtie2Biom.sh
@@ -3,4 +3,6 @@ filename=$(basename "$read_1")
 name=$(echo "$filename" | cut -d '_' -f 1)
 name="${name%%.*}"
 
-metaphlan $1 --nproc $2 --input_type bowtie2out -o ${name}_output.txt --biom ${name}_output.biom  --bowtie2db $3
+time{
+    metaphlan $1 --nproc $2 --input_type bowtie2out -o ${name}_output.txt --biom ${name}_output.biom  --bowtie2db $3
+}
