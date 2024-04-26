6ce815554b7acf9497ce11b0d4050cff8467c389
franky2204 <fpantini1997@gmail.com>
2024-04-26 17:02:49 +0200

    f

--- checkInputContainer/findFiles.sh
+++ checkInputContainer/findFiles.sh
@@ -6,7 +6,7 @@ string=""
 
 # Itera attraverso i file nella cartella
 for file in "$folder_path"/*; do
-	if [[ $file =~ R1 ]]; then
+	if [[ $file =~ R5 ]]; then
 		r2_file="${file/R1/R2}"
 		if [ -f "$r2_file" ]; then
 			file=$(basename "$file")
