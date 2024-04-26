306a1fc6753e912e4a880eed77695f7ed156f06b
franky2204 <fpantini1997@gmail.com>
2024-04-26 17:01:23 +0200

    try

--- checkInputContainer/findFiles.sh
+++ checkInputContainer/findFiles.sh
@@ -4,19 +4,19 @@
 folder_path=$1
 string=""
 
-# Trova i file corrispondenti
-for file in "$folder_path"/*R1.f*; do
-    r2_file="${file/R1.f/R2.f}"
-    if [ -f "$r2_file" ]; then
-        file=$(basename "$file")
-        r2_file=$(basename "$r2_file")
-        if [ -n "$string" ]; then
-            string="$string,\"$file,$r2_file\""
-        else
-            string="\"$file,$r2_file\""
-        fi
-    fi
+# Itera attraverso i file nella cartella
+for file in "$folder_path"/*; do
+	if [[ $file =~ R1 ]]; then
+		r2_file="${file/R1/R2}"
+		if [ -f "$r2_file" ]; then
+			file=$(basename "$file")
+			r2_file=$(basename "$r2_file")
+			if [ -n "$string" ]; then
+				string="$string,\"$file,$r2_file\""
+			else
+				string="\"$file,$r2_file\""
+			fi
+		fi
+	fi
 done
-
-# Stampa la stringa JSON
 echo "{\"value\": [$string]}"
