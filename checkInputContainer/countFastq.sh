304798b42b7107632c7d5e4a86a4ad8a85aa1394
franky2204 <fpantini1997@gmail.com>
2024-04-29 21:14:29 +0200

    fix

--- checkInputContainer/countFastq.sh
+++ checkInputContainer/countFastq.sh
@@ -2,8 +2,11 @@ file=$1
 
 file_fullname=$(basename $file)
 file_name=$(echo ${file_fullname} | cut -d'.' -f1) 
-ext="_count_fastq.txt"
+ext="_count.txt"
 file_name="$file_name$ext"
 
-zcat $1 | wc -l > $file_name
+gzip -d $1
+
+wc -l $unzip1 > $$file_name
+
 
