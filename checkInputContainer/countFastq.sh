b62a9dbba1eba89e8f28f34bd3acef601d9d5a3b
franky2204 <fpantini1997@gmail.com>
2024-04-30 11:50:13 +0200

    t

--- checkInputContainer/countFastq.sh
+++ checkInputContainer/countFastq.sh
@@ -1,12 +1,15 @@
 file=$1
 file_fullname=$(basename $file)
+file1_no_ext=$(basename "$1" .gz)
+file2_no_ext="${file_no_ext/1.f/2.f}"
 file_name=$(echo ${file_fullname} | cut -d'.' -f1) 
 ext="_count.txt"
 file_name="$file_name$ext"
 
 
 
-gzip -d $1 | wc -l > file1.txt
-gzip -d $2 | wc -l > file2.txt
-cat file1.txt file2.txt > $file_name
+gzip -d $1  &&  wc -l $file1_no_ext > text1.txt
+gzip -d $2  &&  wc -l $file2_no_ext > text2.txt
+
+cat text1.txt text2.txt > $file_name
 
