1b79fb83753b953d37882a912fb594778ceccfb0
franky2204 <fpantini1997@gmail.com>
2024-04-30 12:48:57 +0200

    fix bash

--- checkInputContainer/countFastq.sh
+++ checkInputContainer/countFastq.sh
@@ -1,10 +1,10 @@
 file=$1
-file_fullname=$(basename $file)
-file1_no_ext=$(basename "$1" .gz)
-file2_no_ext="${file_no_ext/1.f/2.f}"
-file_name=$(echo ${file_fullname} | cut -d'.' -f1) 
-ext="_count.txt"
-file_namef="$file_name$ext"
+file_fullname=$(basename $file) #file1.f.gz
+file1_no_ext=$(basename "$1" .gz) #/path/file1.f
+file2_no_ext="${file_no_ext/1.f/2.f}" #/path/file2.f
+file_name=$(echo ${file_fullname} | cut -d'.' -f1) #file1(indexes also if applicables).
+ext="_count.txt" 
+file_namef="$file_name$ext" #file_count.txt
 
 
 
