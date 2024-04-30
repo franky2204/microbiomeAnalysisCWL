1644df5eb5373d56888ef509c1e5a8cd2d9e6733
franky2204 <fpantini1997@gmail.com>
2024-04-30 11:21:59 +0200

    fix bash

--- checkInputContainer/countFastq.sh
+++ checkInputContainer/countFastq.sh
@@ -4,8 +4,9 @@ file_name=$(echo ${file_fullname} | cut -d'.' -f1)
 ext="_count.txt"
 file_name="$file_name$ext"
 
-gzip -d $1
 
-gzip -d $1 | wc -l > $file_name
 
+gzip -d $1 | wc -l > file1.txt
+gzip -d $2 | wc -l > file2.txt
+cat file1.txt file2.txt > $file_name
 
