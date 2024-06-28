410d22d2cda038579d16c1c8fba7fe4f1a0a666e
franky2204 <fpantini1997@gmail.com>
2024-06-28 10:39:08 +0200

    fix

--- humanMapperContainer/scripts/fuseReads.sh
+++ humanMapperContainer/scripts/fuseReads.sh
@@ -1,7 +1,7 @@
-$filename1=$(basename "$1")
-$name1=$(echo "$filename" | cut -d '.' -f 1)
-$filename2=$(basename "$2")
-$name2=$(echo "$filename2" | cut -d '.' -f 1)
+filename1=$(basename "$1")
+name1=$(echo "$filename" | cut -d '.' -f 1)
+filename2=$(basename "$2")
+name2=$(echo "$filename2" | cut -d '.' -f 1)
 unpigz $1
 unpigz $2
 cat $name1.fastq $name2.fastq > ${name1}_fused.fastq
