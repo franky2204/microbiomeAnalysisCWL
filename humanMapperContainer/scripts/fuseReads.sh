06a7f167a431776c9e2526ace0ba82df5697bf83
franky2204 <fpantini1997@gmail.com>
2024-07-02 11:16:03 +0200

    fix fuse file

--- humanMapperContainer/scripts/fuseReads.sh
+++ humanMapperContainer/scripts/fuseReads.sh
@@ -1,7 +1,13 @@
+remove_after_last_dot() {
+    local str="$1"
+    local result="${str%.*}"
+    echo "$result"
+}
+
 filename1=$(basename "$1")
-name1="${filename1%%.*}"
+name1=$(remove_after_last_dot $filename1)
 filename2=$(basename "$2")
-name2="${filename2%%.*}"
+name2=$(remove_after_last_dot $filename2)
 unpigz $1
 unpigz $2
 cat $name1.fastq $name2.fastq > ${name1}_fused.fastq
