aedb570f73fd0b38b32b39b7947904e6effbc0ab
franky2204 <fpantini1997@gmail.com>
2024-07-10 16:24:39 +0200

    fix fastcount

--- humanMapperContainer/scripts/countFast.sh
+++ humanMapperContainer/scripts/countFast.sh
@@ -4,9 +4,17 @@ file1=$1
 file2=$2
 output_file=$3
 
-
 lines_file1=$(wc -l < "$file1")
 lines_file2=$(wc -l < "$file2")
 
-echo "$file1: $lines_file1" >> "$output_file"
-echo "$file2: $lines_file2" >> "$output_file"
+quarter_lines_file1=$((lines_file1 / 4))
+quarter_lines_file2=$((lines_file2 / 4))
+
+file1_basename=$(basename "$file1")
+file2_basename=$(basename "$file2")
+
+file1_nameroot="${file1_basename%.*}"
+file2_nameroot="${file2_basename%.*}"
+
+echo "$file1_nameroot: $quarter_lines_file1" >> "$output_file"
+echo "$file2_nameroot: $quarter_lines_file2" >> "$output_file"
