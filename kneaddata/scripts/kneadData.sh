cd507cc28560c785610e0fb8933041f54b243719
franky2204 <fpantini1997@gmail.com>
2024-09-04 09:49:52 +0200

    fix names

--- kneaddata/scripts/kneadData.sh
+++ kneaddata/scripts/kneadData.sh
@@ -11,14 +11,11 @@ extension="${extension##*.}"
 
 unzip1="${remEx1}.${extension}"
 unzip2="${remEx2}.${extension}"
-echo "remEX1 = $remEx1"
-echo "remEX2 = $remEx2"
-echo "remEX2 = $unzip1"
 
 time {
     unpigz $read1
     unpigz $read2
-    kneaddata --input $unRead1 --input $unRead2 --reference-db $db_path --threads $threads --output ./ 
+    kneaddata --input $unzip1 --input $unzip2 --reference-db $db_path --threads $threads --output ./ 
     mv ${remEx1}_kneaddata_paired_1.${extension} ${remEX1}_out.fastq
     mv ${remEx1}_kneaddata_paired_2.${extension} ${remEX2}_out.fastq
     pigz -t $threads ${remEX1}_out.fastq
