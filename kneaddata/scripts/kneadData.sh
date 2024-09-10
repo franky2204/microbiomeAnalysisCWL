e52988a774bcd698fca2676bbb0701b8d38faa7f
franky2204 <fpantini1997@gmail.com>
2024-09-10 20:41:03 +0200

    fix name without db

--- kneaddata/scripts/kneadData.sh
+++ kneaddata/scripts/kneadData.sh
@@ -16,8 +16,10 @@ time {
     unpigz $read1
     unpigz $read2
     kneaddata --input $unzip1 --input $unzip2  --threads $threads --output ./ 
-    mv ${remEx1}_kneaddata_paired_1.${extension} ${remEx1}_out1.fastq
-    mv ${remEx1}_kneaddata_paired_2.${extension} ${remEx2}_out2.fastq
+    #mv ${remEx1}_kneaddata_paired_1.${extension} ${remEx1}_out1.fastq
+    mv ${remEx1}_kneaddata.repeats.removed.1.${extension} ${remEx1}_out1.fastq
+    #mv ${remEx1}_kneaddata_paired_2.${extension} ${remEx2}_out2.fastq
+    mv ${remEx1}_kneaddata.repeats.removed.2.${extension} ${remEx2}_out2.fastq
     pigz -p $threads ${remEx1}_out1.fastq
     pigz -p $threads ${remEx2}_out2.fastq
 }
