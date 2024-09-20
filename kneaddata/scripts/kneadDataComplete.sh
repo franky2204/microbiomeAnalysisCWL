9bf65d6bafa11d9b19fc7e45274899a1a2ba4e44
franky2204 <fpantini1997@gmail.com>
2024-09-20 11:17:41 +0200

    fixx

--- kneaddata/scripts/kneadDataComplete.sh
+++ kneaddata/scripts/kneadDataComplete.sh
@@ -10,7 +10,7 @@ remEx2="${onlyname2%%.*}"
 time {
     unpigz $read1
     unpigz $read2
-    kneaddata --input $unzip1 --input $unzip2  --reference-db $db_path --threads $threads --output ./ 
+    kneaddata -i1 $unzip1 -i2 $unzip2  -db $db_path -t $threads -o ./ 
     mv ${remEx1}_kneaddata_paired_1.fastq ${remEx1}_out1.fastq
     mv ${remEx1}_kneaddata_paired_2.fastq ${remEx2}_out2.fastq
     pigz -p $threads ${remEx1}_out1.fastq
