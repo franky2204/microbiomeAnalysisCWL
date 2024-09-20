6dd24c72e875db0dfdfc80b9784883b3eba43333
franky2204 <fpantini1997@gmail.com>
2024-09-20 11:12:16 +0200

    fix name

--- kneaddata/scripts/kneadDataComplete.sh
+++ kneaddata/scripts/kneadDataComplete.sh
@@ -6,18 +6,13 @@ onlyname1="${read1##*/}"
 onlyname2="${read2##*/}"
 remEx1="${onlyname1%%.*}"
 remEx2="${onlyname2%%.*}"
-extension="${onlyname2%.*}"
-extension="${extension##*.}"
-
-unzip1="${remEx1}.${extension}"
-unzip2="${remEx2}.${extension}"
 
 time {
     unpigz $read1
     unpigz $read2
     kneaddata --input $unzip1 --input $unzip2  --reference-db $db_path --threads $threads --output ./ 
-    mv ${remEx1}_kneaddata_paired_1.${extension} ${remEx1}_out1.fastq
-    mv ${remEx1}_kneaddata_paired_2.${extension} ${remEx2}_out2.fastq
+    mv ${remEx1}_kneaddata_paired_1.fastq ${remEx1}_out1.fastq
+    mv ${remEx1}_kneaddata_paired_2.fastq ${remEx2}_out2.fastq
     pigz -p $threads ${remEx1}_out1.fastq
     pigz -p $threads ${remEx2}_out2.fastq
 }
