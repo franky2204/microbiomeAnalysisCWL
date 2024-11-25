e2eafa91c89a9a6b57965c907bf2e6ea8436752e
franky2204 <fpantini1997@gmail.com>
2024-11-25 10:58:08 +0100

    pre KHumann done

--- checkInputContainer/findReportUni.sh
+++ checkInputContainer/findReportUni.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
 string=$1
 directory=$2
-stringR1= basename $string | grep -oP '.*(?=_)'
-echo $stringR1
+
+
+stringR1=$(basename "$string")
+stringR1="${stringR1%%_*}"
+echo "Stringa elaborata: $stringR1"
+
 for file in "$directory"/*; do
-    if [[ $file =~$stringR1.report ]]; then
-        cp $file .
+    if [[ "$(basename "$file")" == "${stringR1}.report" ]]; then
+        echo "Trovato file: $file"
+        cp "$file" .
     fi
 done
+
