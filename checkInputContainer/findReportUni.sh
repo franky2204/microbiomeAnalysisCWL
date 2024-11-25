6f2f59487f128dec93fe3e6667d6afdd60f91c47
franky2204 <fpantini1997@gmail.com>
2024-11-25 10:20:02 +0100

    try

--- checkInputContainer/findReportUni.sh
+++ checkInputContainer/findReportUni.sh
@@ -1,8 +1,9 @@
 string=$1
 directory=$2
 stringR1= basename $string | grep -oP '.*(?=_)'
-for file in $directory/*; do
-    if [[ $file == *$stringR1* ]]; then
+echo $stringR1
+for file in "$directory"/*; do
+    if [[ $file =~$stringR1.report ]]; then
         cp $file .
     fi
 done
